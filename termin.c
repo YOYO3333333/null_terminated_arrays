6896d22572652e779dea1f1d090bbf6b3dc6456f
YOYO3333333 <114952052+YOYO3333333@users.noreply.github.com>
2022-11-24 19:02:54 +0100

    Create termin.c

---
+++ termin.c
@@ -0,0 +1,41 @@
+#include "null_terminated_arrays.h"
+
+#include <stddef.h>
+
+size_t len_array(const char ***arr, size_t i)
+{
+    size_t LEN = 0;
+    if (arr[i])
+    {
+        while (arr[i][LEN])
+            j++;
+    }
+    return j;
+}
+void reverse_matrix(const char ***matrix)
+{
+    size_t len = 0;
+    while (*(matrix + len))
+        len++;
+    size_t i = 0;
+    for (size_t i = 0;i < len;i++)
+    {
+        size_t len2 = len_array(matrix, i);
+        size_t k = 0;
+        while (k < len2 / 2)
+        {
+            const char *tmp = matrix[i][k];
+            matrix[i][k] = matrix[i][len2 - k - 1];
+            matrix[i][len2 - k - 1] = tmp;
+            k++;
+        }
+    }
+    i = 0;
+    while (size_t i = 0;i < len / 2;i ++)
+    {
+        const char **tmp = matrix[i];
+        matrix[i] = matrix[len - i - 1];
+        matrix[len - i - 1] = tmp;
+    }
+    return;
+}
